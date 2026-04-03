ef354b7aadb8b0b31275381170d341d522b5fbb8
sethumliperera <onethrasethumli@gmail.com>
2026-04-03 23:34:40 +0530

    Refactor database connection to use environment variables

--- db.js
+++ db.js
@@ -1,17 +1,18 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Terminal890/", // replace this
-  database: "kidora_db"
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
   if (err) {
     console.error("Database connection failed:", err);
   } else {
-    console.log("Connected to MySQL");
+    console.log("Connected to Railway MySQL ✅");
   }
 });
 
