baeaf4b8bad6982f18cfe52447a34932041df835
sethumliperera <onethrasethumli@gmail.com>
2026-04-04 17:13:23 +0530

    Implement check for DATABASE_URL existence
    Add error handling for missing DATABASE_URL environment variable.

--- db.js
+++ db.js
@@ -1,5 +1,10 @@
 const mysql = require("mysql2");
 
+if (!process.env.DATABASE_URL) {
+  console.error("❌ DATABASE_URL is missing!");
+  process.exit(1);
+}
+
 const db = mysql.createPool(process.env.DATABASE_URL).promise();
 
 module.exports = db;
